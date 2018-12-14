61425a69a840f316ccad256abe0bf03af5273847
Matthew Kirk <matt@matthewkirk.com>
2018-12-13 19:54:48 -0800

    feat: finish off updating the naive bayesian classification stuff

--- naive_bayes/__init__.py
+++ naive_bayes/__init__.py
@@ -1,4 +1,4 @@
-from naive_bayes.tokenizer import Tokenizer
+# from naive_bayes.tokenizer import Tokenizer
 from naive_bayes.email_object import EmailObject
-from naive_bayes.spam_trainer import SpamTrainer
+# from naive_bayes.spam_trainer import SpamTrainer
 __all__ = [ "tokenizer" ]
